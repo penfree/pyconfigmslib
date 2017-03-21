44746b79234d56029fe86d8ba1598681b47cbe3c
penfree <qiupengfei@iyoudoctor.com>
2017-03-21 13:30:27 +0800

    add hdfs section

--- configmslib/sections/_hdfs.py
+++ configmslib/sections/_hdfs.py
@@ -18,47 +18,49 @@ class HDFSConfigSection(ReferConfigSection):
                   port: 8020 (default)
                 - host: namenode2.domain
                   port: 8020 (default)
-            use_trash: false (default)
             user: hdfs (default)
-            timeout: 30 (default, in seconds)
     '''
     Type = 'hdfs'
     ReloadRequired = True
     DefaultPort = 8020
-    DefaultUseTrash = False
     DefaultUser = 'hdfs'
-    DefaultTimeout = 30
 
     def validate(self, value):
         if not value.get('hosts'):
             raise ValueError('hosts required')
         else:
             for host in value.get('hosts'):
-                if not value.get('host'):
+                if not host.get('host'):
                     raise ValueError('host required')
     
     def reference(self, config):
         '''
             Create hdfs connection
         '''
-        from snakebite.client import HAClient
-        from snakebite.client import Namenode
+        host = ''
+        port = None
+        pars = {}
+        if len(config.get('hosts')) > 1:
+            # HA mode
+            host = 'nn'
+            pars = {'dfs.nameservices': 'nn'}
+            namenodes = []
+            for idx, item in enumerate(config['hosts'], start=1):
+                namenodes.append('nn%d' % idx)
+                pars['dfs.namenode.rpc-address.nn.nn%d' % idx] = "{host}:{port}".format(host=item['host'], port=item.get('port', self.DefaultPort))
+            pars['dfs.ha.namenodes.nn'] = ','.join(namenodes)
+        else:
+            # Single namenode mode
+            host = config['hosts'][0]['host']
+            port = config['hosts'][0].get('port', self.DefaultPort)
 
-        use_trash = config.get('use_trash', False)
-        user = config.get('user', self.DefaultUser)
-        timeout = config.get('timeout', self.DefaultTimeout)
-
-        namenodes = [
-            Namenode(host['host'], host.get('port', self.DefaultPort)) for host in config['hosts']
-        ]
-
-        client = HAClient(namenodes, use_trash, effective_user=user, sock_request_timeout=timeout)
-
-        return client
+        import hdfs3
+        fs = hdfs3.HDFileSystem(host=host, port=port, pars=pars, user=config.get('user', self.DefaultUser))
+        return fs
 
     def release(self, value):
         '''
             release connection
         '''
         # Nothing to do
-        pass
+        value.disconnect()
